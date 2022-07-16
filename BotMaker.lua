8b01c40e5a1b94043cb3de76ac5d540363e78842
Momo <84031783+LGDMomo@users.noreply.github.com>
2022-07-16 19:15:22 +0200

    Update BotMaker.lua

--- BotMaker.lua
+++ BotMaker.lua
@@ -1,21 +1,70 @@
+repeat task.wait() until game:IsLoaded()
+
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Bot Gucci 💰", "BloodTheme")
---nofall
+--nofall fim
 --game.Players.LocalPlayer.Character.CharacterHandler.Remotes.ApplyFallDamage:Destroy()
 --main tab
 local main = Window:NewTab("Main")
 local mainsection = main:NewSection("Bot Maker Made By Momo.#2706")
 --variables
-local posi = {}
 local waittime = 0
 local tweenspeed = 200 --default speed
 local savename = "exported bot config"
 local bottxtimport = nil
 local launched = false
 local loopspeed = 2 --default speed
-local IsLooped = false
-local canServerhop = false
-local AutoTrinkets= false
+local posi = {}
+--config for save and load file 
+local file_name = "config.txt"
+--basic table for saving config
+local ConfigTable = {
+    IsLooped = false,
+    canServerhop = false,
+    AutoTrinkets= false,
+    ServerHopPos = false,
+}
+--load config function
+function loadSettings()
+    print("Loading config")
+    local HttpService = game:GetService("HttpService")
+    if (readfile and isfile and isfile(file_name)) then
+        ConfigTable = HttpService:JSONDecode(readfile(file_name));
+        --reading temp pos file for bot
+        posi = {}
+        local file = readfile("TempPos"..".txt")
+        for word in string.gmatch(file, '([^a]+)') do
+            table.insert(posi,(word))
+        end
+        print("Loaded config")
+    else
+        print("Error while loading config")
+    end
+end
+--save config function
+function saveSettings()
+    print("saving config")
+    local json;
+    local HttpService = game:GetService("HttpService");
+    if(writefile) then
+        json = HttpService:JSONEncode(ConfigTable);
+        writefile(file_name, json);
+    --saving position table in a separate file
+    writefile("TempPos"..".txt","")
+    for i , v in pairs(posi)do
+        if i == #posi - 0 then 
+            appendfile("TempPos"..".txt",tostring(v).."a")
+            else
+            appendfile("TempPos"..".txt",tostring(v).."a".."\n")
+        end
+    end
+        else
+            print("Error while saving config")
+    end
+end
+--on join we load the config
+loadSettings()
+
 --Tabs
 local boted = Window:NewTab("Bot")
 local FileSave = Window:NewTab("Save File")
@@ -38,7 +87,7 @@ paramssection:NewSlider("Tween Speed", "", 750, 1, function(s)
     tweenspeed = s
 end)
 
-
+--block player function for server hopping
 block_random_player = function()
     local block_player 
     local players_list = game:GetService("Players"):GetPlayers()
@@ -78,9 +127,9 @@ end
 
 local TweenPlayer
 --main functions
-local function farm()
+function farm()
     launched = true
-    local function tweenplayer(cframe)
+    function tweenplayer(cframe)
         TweenPlayer = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(((cframe.p - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude)/tweenspeed, Enum.EasingStyle.Linear, Enum.EasingDirection.Out, 0, false), {CFrame = cframe})
         TweenPlayer:Play()
         TweenPlayer.Completed:Wait() wait(1)
@@ -95,21 +144,26 @@ local function farm()
             tweenplayer(CFrame.new(unpack(v:split(", "))))
             else
                 tweenplayer(CFrame.new(v))
+                wait(waittime)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
+                wait(waittime)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
             end
             wait(waittime)
             game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
         end
     end
     launched = false
-    wait(1)
-    if canServerhop then
+    if ConfigTable.canServerhop then
         servhop()
     end
+    wait(2)
 end
 --save position    
 botedsection:NewButton("Save Position","()",function()
     local pos = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
     table.insert(posi,pos)
+    saveSettings()
 end)
 --start bot
 botedsection:NewButton("Load Position","()",function()
@@ -118,18 +172,25 @@ end)
 --remove all position
 botedsection:NewButton("Clear All Pos","()",function()
     posi = {}
+    saveSettings()
 end)
 --loop
-botedsection:NewToggle("Loop Bot On/Off","()",function(state)
-   IsLooped = state
+botedsection:NewToggle("Loop Bot On/Off (Single Server)","()",function(state)
+   ConfigTable.IsLooped = state
+   saveSettings()
 end)
---set boolean for serverhopping
-botedsection:NewToggle("Server Hop On/Off","()",function(state)
-    canServerhop = state
+
+--server hop mode
+botedsection:NewToggle("Bot Then Server Hop","()",function(state)
+    ConfigTable.ServerHopPos = state
+    ConfigTable.canServerhop = state
+    saveSettings()
  end)
+
 --auto trinkets fim
  botedsection:NewToggle("Auto Pickup (fim)","()",function(state)
-    AutoTrinkets = state
+    ConfigTable.AutoTrinkets = state
+    saveSettings()
  end)
 --server hop
 botedsection:NewButton("Server Hop", "Server Hop", function()
@@ -140,11 +201,13 @@ botedsection:NewButton("Server Hop", "Server Hop", function()
 filesection:NewTextBox("Export File Name", "", function(txt)
     savename = txt
     print(savename)
+    saveSettings()
 end)
 --import file name
 filesection:NewTextBox("Import File Name", "", function(txt)
     bottxtimport = txt
     print(savename)
+    saveSettings()
 end)
 --export file button
 filesection:NewButton("Export bot file", "", function(txt)
@@ -156,7 +219,9 @@ filesection:NewButton("Export bot file", "", function(txt)
             appendfile(tostring(savename)..".txt",tostring(v).."a".."\n")
         end
     end
+    saveSettings()
 end)
+--import file bot config
 filesection:NewButton("Import Bot file", "", function(txt)
     posi = {}
     local file = readfile(tostring(bottxtimport)..".txt")
@@ -166,10 +231,12 @@ filesection:NewButton("Import Bot file", "", function(txt)
     for i,v in pairs(posi)do
         print(v)
     end
+    saveSettings()
 end)
+
 --auto loop farm
 game:GetService("RunService").RenderStepped:Connect(function()
-	if IsLooped then
+	if ConfigTable.IsLooped then
 		if not launched then
             launched = true
             wait(loopspeed)
@@ -177,8 +244,20 @@ game:GetService("RunService").RenderStepped:Connect(function()
         end
 	end
 end)
+--loop for server hop bot (momo was here :p)
 game:GetService("RunService").RenderStepped:Connect(function()
-	if AutoTrinkets then
+	if ConfigTable.ServerHopPos then
+        if not launched then
+            launched = true
+            farm()
+            wait(9e9)
+        end
+    end
+end)
+
+--auto pickup trinkets
+game:GetService("RunService").RenderStepped:Connect(function()
+	if ConfigTable.AutoTrinkets then
         for i,v in pairs(game:GetService("Workspace").Trinkets:GetDescendants())do
             if v.ClassName == "ClickDetector" then
                 local distance = (v.Parent.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude
@@ -189,3 +268,5 @@ game:GetService("RunService").RenderStepped:Connect(function()
         end
 	end
 end)
+--save settings on join
+saveSettings()
